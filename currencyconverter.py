0b7a5adec545545ecf113dbf512d81413f64b521
brian (the robot off of the confused.com adverts (he will save you up to 100 pounds on your car insurance)) <10jknight@ashmanorstudent.com>
2014-06-11 11:26:58 +0100

    task 1

---
+++ currencyconverter.py
@@ -0,0 +1,85 @@
+allowables = ["dollars", "pounds", "euro", "yen"]
+pounds = 'pounds'
+dollars = 'dollars'
+yen = 'yen'
+euro = 'euro'
+print("Welcome to the currency converter")
+var1 = input("Please type what currency you wish to convert from ")
+var2 = input("Please type the currency that you wish to convert too ")
+var3 = float(input("Please type the amount of currency you wish to convert "))
+#[GBP]
+if var1 == pounds and var2 == dollars:
+    sumusd1 = var3 * 1.64
+    print("You will recieve, ", sumusd)
+elif something != somethingelse: 
+    print("Sorry, an error has occurred, please input a currency")
+    print(var1)
+if var1 == pounds and var2 == euro:
+    sumeuro = var3 * 1.22
+    print("You will recieve, ", sumeuro)
+elif something != somethingelse:
+    print("Sorry, an error has occurred, please input a currency")
+    print(var1)
+if var1 == pounds and var2 == yen:
+    sumyen = var3 * 171.53
+    print("You will recieve, ", sumyen)
+elif something != somethingelse:
+    print("Sorry, an error has occurred, please input a currency")
+    print(var1)
+#[USD]
+if var1 == dollars and var2 == pounds:
+    sumpounds = var3 * 0.61
+    print("You will recieve, ", sumpounds)
+elif something != somethingelse:
+    print("Sorry, an error has occurred, please input a currency")
+    print(var1)
+if var1 == dollars and var2 == euro:
+    sumeuro = var3 * 0.74
+    print("You will recieve, ", sumeuro)
+elif something != somethingelse:
+    print("Sorry, an error has occurred, please input a currency")
+    print(var1)
+if var1 == dollars and var2 == yen:
+    sumyen = var3 * 104.16
+    print("You will recieve, ", sumyen)
+elif something != somethingelse:
+    print("Sorry, an error has occurred, please input a currency")
+    print(var1)
+#[EURO]
+if var1 == euro and var2 == dollars:
+    sumusd2 = var3 * 1.64
+    print("You will recieve, ", sumusd)
+elif something != somethingelse:
+    print("Sorry, an error has occurred, please input a currency")
+    print(var1)
+if var1 == euro and var2 == pounds:
+    sumpounds = var3 * 0.82
+    print("You will recieve, ", sumpounds)
+elif something != somethingelse:
+    print("Sorry, an error has occurred, please input a currency")
+    print(var1)
+if var1 == euro and var2 == yen:
+    sumyen = var3 * 141.24
+    print("You will recieve, ", sumyen)
+elif something != somethingelse:
+    print("Sorry, an error has occurred, please input a currency")
+    print(var1)
+#[YEN]
+    if var1 == yen and var2 == dollars:
+        sumusd3 = var3 * 0.0096
+    print("You will recieve, ", sumusd3)
+elif something != somethingelse:
+    print("Sorry, an error has occurred, please input a currency")
+    print(var1)
+if var1 == yen and var2 == euro:
+    sumeuro = var3 * 0.0071
+    print("You will recieve, ", sumeuro)
+elif something != somethingelse:
+    print("Sorry, an error has occurred, please input a currency")
+    print(var1)
+if var1 == yen and var2 == pounds:
+    sumpounds = var3 * 0.0058
+    print("You will recieve, ", sumpounds)
+elif something != somethingelse:
+    print("Sorry, an error has occurred, please input a currency")
+    print(var1)
